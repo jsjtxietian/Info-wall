190de113a1d5496ecca61268dc62cb4097dcf534
jsjtxietian <jsjtxietian@outlook.com>
2020-01-02 15:02:05 +0800

    G6

--- src/App.js
+++ src/App.js
@@ -1,3 +1,46 @@
-import ChangeLayout from './ChangeLayout'; 
+import React, { useEffect, useState } from 'react';
+import ReactDOM from 'react-dom';
+import { data } from './data';
+import G6 from '@antv/g6';
 
-export default ChangeLayout;
+export default function() {
+  const ref = React.useRef(null);
+  let graph = null;
+
+  useEffect(() => {
+    if (!graph) {
+      graph = new G6.Graph({
+        container: ReactDOM.findDOMNode(ref.current),
+        width: 1200,
+        height: 800,
+        modes: {
+          default: ['drag-canvas'],
+        },
+        layout: {
+          type: 'dagre',
+          direction: 'LR',
+        },
+        defaultNode: {
+          shape: 'node',
+          labelCfg: {
+            style: {
+              fill: '#000000A6',
+              fontSize: 10,
+            },
+          },
+          style: {
+            stroke: '#72CC4A',
+            width: 150,
+          },
+        },
+        defaultEdge: {
+          shape: 'polyline',
+        },
+      });
+    }
+    graph.data(data);
+    graph.render();
+  }, []);
+
+  return <div ref={ref}></div>;
+}
